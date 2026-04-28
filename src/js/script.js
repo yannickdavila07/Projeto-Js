2aafcc855707beae7d592293e98819d56ef09edb
Yannick <davilayannick07@gmail.com>
2026-04-28 11:27:02 -0300

    feat: Métodos tofixad template string e concatenação

--- src/js/script.js
+++ src/js/script.js
@@ -28,3 +28,13 @@ console.log(texto2)
 const texto3 = texto2.join("-");
 console.log(texto3)
 
+// metodo toFixad
+
+const precoProduto = 199.99;
+const desconto = 0.15 //15%
+const precoFinal = precoProduto * (1 - desconto)
+
+console.log(precoFinal);
+
+// `` - template strings - concatenação
+console.log(`R$ ${precoFinal.toFixed(2)}`)
