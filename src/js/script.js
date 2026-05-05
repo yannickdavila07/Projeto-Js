dc071b805e5128188edc19e5604e708924954d3d
Yannick <davilayannick07@gmail.com>
2026-05-05 11:50:54 -0300

    feat: style: paleta de cores com variaveis e estilização do card

--- src/js/script.js
+++ src/js/script.js
@@ -46,3 +46,53 @@ const titulo = document.getElementById('titulo');
 titulo.innerText = "O DOM Mudou o Texto";
 
 titulo.style.color = "blue"
+
+
+/* CRIANDO O APP */
+const produtos =[
+    {nome: "Teclado Mecânico", preco:300, emPromocao: true},
+    {nome: "Mouse Gamer", preco:150, emPromocao: false},
+    {nome: "Monitor Gamer", preco:1300, emPromocao: true},
+    {nome: "Pad Mouse XL", preco:400, emPromocao: false},
+
+];
+
+const container = document.getElementById("listar-produtos");
+const mostrarTotal = document.getElementById("resultado-total");
+
+// Map: transforma o array de objetos em uma array de strings html e css
+
+function todosProdutos(lista){
+    mostrarTotal.innerText = "";
+    const htmlProdutos = lista.map(item =>` 
+        <div class="card">
+            <h3>${item.nome} </h3>
+            <p>Preço: R$ ${item.preco}</p>
+            ${item.emPromocao ? '<span class = "tag">Promoção</span>'
+                : ''}
+        </div>
+        
+        `).join(''); //transforma o array em uma unica lista
+
+        container.innerHTML= htmlProdutos;
+
+}
+
+
+function filtrarPromocoes(lista){
+    const promocionais = produtos.filter(item => item.emPromocao);
+    todosProdutos(promocionais);
+
+}
+
+//Filter - cria uma nova lista apenas com o que selecionou
+
+//REDUCE - Reduz o array a um unico valor(soma dos precos)
+
+function calcularTotal(){
+    const total = produtos.reduce((acumulador,item) =>{
+        return acumulador + item.preco;
+    }, 0);
+
+mostrarTotal.innerText = `Valor Total R$ ${total}`;
+}
